863d0a099378e04bc63b7b06a0e0c6affe29839a
prosenjit barman <prosenjitbarmann@gmail.com>
2019-11-02 21:38:06 +0600

    Update metas.php
    Metas to handle https request

--- application/views/frontend/default/metas.php
+++ application/views/frontend/default/metas.php
@@ -1,7 +1,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<meta name="robots" content="index, follow">
-<meta name="description" content="Bayanno Hospital Management System">
-<meta name="keywords" content="bootstrap, responsive, template, website, html, theme, ux, ui, web, design, developer, support, business, corporate, real estate, education, medical, school, education, demo, css, framework">
-<meta name="author" content="Creativeitem">
+<meta name="author" content="Team Pihs">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
